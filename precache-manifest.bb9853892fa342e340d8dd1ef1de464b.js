c9d93608b18f72bab0e5a972c544f4f97e3b4a6c
nanben <nanta.benellem@gmail.com>
2020-02-19 16:17:50 +0100

    Updates

---
+++ precache-manifest.bb9853892fa342e340d8dd1ef1de464b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "98387d2f9dcdbf6f0510",
+    "url": "/nanben/spongah_app.git/static/css/main.9e138378.chunk.css"
+  },
+  {
+    "revision": "98387d2f9dcdbf6f0510",
+    "url": "/nanben/spongah_app.git/static/js/main.d4a872af.chunk.js"
+  },
+  {
+    "revision": "2860c5e53e97016fae0d",
+    "url": "/nanben/spongah_app.git/static/js/runtime~main.1d49f34b.js"
+  },
+  {
+    "revision": "62d4242a875a6dc2a41b",
+    "url": "/nanben/spongah_app.git/static/css/2.90be059f.chunk.css"
+  },
+  {
+    "revision": "62d4242a875a6dc2a41b",
+    "url": "/nanben/spongah_app.git/static/js/2.f33116c6.chunk.js"
+  },
+  {
+    "revision": "745bdcc1db54199c8c97692c6003ea31",
+    "url": "/nanben/spongah_app.git/static/media/iconPasGrave.745bdcc1.svg"
+  },
+  {
+    "revision": "c3ef0129b740c71810ff66cf0dd66f7a",
+    "url": "/nanben/spongah_app.git/static/media/iconFatal.c3ef0129.svg"
+  },
+  {
+    "revision": "23d501db1647f6da12818ca95cef67d8",
+    "url": "/nanben/spongah_app.git/static/media/iconBlessure.23d501db.svg"
+  },
+  {
+    "revision": "c9ce8723e8de38a1c49f6259c6d2e754",
+    "url": "/nanben/spongah_app.git/static/media/icon.c9ce8723.png"
+  },
+  {
+    "revision": "556fd88a766013f8cbd3313f0115f67b",
+    "url": "/nanben/spongah_app.git/static/media/imge.556fd88a.png"
+  },
+  {
+    "revision": "2106162d2e15b365c15c14efe8f75a0a",
+    "url": "/nanben/spongah_app.git/index.html"
+  }
+];
